4d559cac119304783d844654715065960bc9ce85
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2023-11-14 15:34:07 -0300

    cambios

---
+++ Tps/TP2/server_a.py
@@ -0,0 +1,88 @@
+import argparse
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+from io import BytesIO
+import socket
+from PIL import Image
+
+def procesar_imagen_entrada(archivo_imagen, factor_escala):
+    try:
+        bytes_imagen = archivo_imagen.read()
+        imagen_pil = Image.open(BytesIO(bytes_imagen))
+
+        print(f"Servidor: Recibida imagen del cliente. Dimensiones originales: {imagen_pil.size}")
+
+        if imagen_pil.mode != 'L':
+            imagen_gris = imagen_pil.convert('L')
+        else:
+            imagen_gris = imagen_pil
+
+        with BytesIO() as salida:
+            imagen_gris.save(salida, format='JPEG')
+            imagen_final = salida.getvalue()
+
+        return imagen_final
+
+    except Exception as error:
+        print(f"Error procesando la imagen: {error}")
+
+def enviar_imagen_final(socket, imagen_bytes_final):
+    try:
+        socket.sendall(len(imagen_bytes_final).to_bytes(4, byteorder='big'))
+        socket.sendall(imagen_bytes_final)
+    except Exception as error:
+        print(f"Error enviando la imagen procesada: {error}")
+
+class ManejadorImagenes(SimpleHTTPRequestHandler):
+    def do_POST(self):
+        try:
+            longitud_contenido = int(self.headers['Content-Length'])
+            bytes_imagen = self.rfile.read(longitud_contenido)
+
+            imagen_final = procesar_imagen_entrada(BytesIO(bytes_imagen), factor_escala=0.5)
+
+            print(f"Servidor: Enviando imagen al servidor de escalado...")
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_conexion:
+                direccion_escalado = ('localhost', 5051)
+                socket_conexion.connect(direccion_escalado)
+                enviar_imagen_final(socket_conexion, imagen_final)
+
+                longitud_datos = int.from_bytes(socket_conexion.recv(4), byteorder='big')
+                bytes_imagen_escalada = socket_conexion.recv(longitud_datos)
+
+                print("Servidor: Recibida imagen escalada del servidor de escalado.")
+
+            nombre_archivo_escalada_principal = "imagen_procesada.jpg"
+            with open(nombre_archivo_escalada_principal, 'wb') as archivo:
+                archivo.write(bytes_imagen_escalada)
+
+            self.send_response(200)
+            self.send_header('Content-type', 'image/jpeg')
+            self.end_headers()
+            self.wfile.write(bytes_imagen_escalada)
+
+            print("Servidor: Imagen procesada enviada al cliente y almacenada localmente.")
+
+        except Exception as error:
+            print(f"Error manejando la solicitud POST: {error}")
+
+def iniciar_servidor(direccion_ip, puerto_escucha):
+    manejador = ManejadorImagenes
+
+    with HTTPServer((direccion_ip, puerto_escucha), manejador) as servidor_http:
+        print(f"Servidor escuchando en {direccion_ip}:{puerto_escucha}")
+        servidor_http.serve_forever()
+
+def main():
+    analizador = argparse.ArgumentParser(description='Procesamiento de imágenes')
+    analizador.add_argument('-i', '--ip', required=True, help='Dirección de escucha')
+    analizador.add_argument('-p', '--puerto', type=int, required=True, help='Puerto de escucha')
+    argumentos = analizador.parse_args()
+
+    try:
+        iniciar_servidor(argumentos.ip, argumentos.puerto)
+    except KeyboardInterrupt:
+        print('Deteniendo el servidor.')
+
+if __name__ == '__main__':
+    main()
---
+++ Tps/TP2/server_b.py
@@ -0,0 +1,84 @@
+import argparse
+import socket
+from PIL import Image
+from io import BytesIO
+import threading
+
+def redimensionar_imagen(imagen, factor_redimension):
+    nueva_dimension = tuple(int(dim * factor_redimension) for dim in imagen.size)
+    imagen_redimensionada = imagen.resize(nueva_dimension, Image.ANTIALIAS)
+    return imagen_redimensionada
+
+def manejar_cliente_redimension(socket_cliente, factor_redimension):
+    try:
+        longitud_datos = int.from_bytes(socket_cliente.recv(4), byteorder='big')
+        imagen_bytes = socket_cliente.recv(longitud_datos)
+
+        imagen_pillow = Image.open(BytesIO(imagen_bytes))
+        
+        print(f"Servidor: Recibida imagen del servidor principal. Dimensiones: {imagen_pillow.size}")
+
+        imagen_redimensionada = redimensionar_imagen(imagen_pillow, factor_redimension)
+
+        print(f"Servidor: Dimensiones después de redimensionar: {imagen_redimensionada.size}")
+
+        with BytesIO() as output:
+            imagen_redimensionada.save(output, format='JPEG')
+            imagen_bytes_redimensionada = output.getvalue()
+
+        enviar_imagen_redimensionada(socket_cliente, imagen_bytes_redimensionada)
+    except Exception as e:
+        print(f"Error manejando el cliente de redimension: {e}")
+    finally:
+        socket_cliente.close()
+
+def enviar_imagen_redimensionada(sock, imagen_bytes_redimensionada):
+    try:
+        longitud_datos = len(imagen_bytes_redimensionada)
+        sock.sendall(longitud_datos.to_bytes(4, byteorder='big'))
+        sock.sendall(imagen_bytes_redimensionada)
+        print("Servidor: Imagen redimensionada enviada al servidor principal.")
+
+    except BrokenPipeError:
+        print("La conexión fue cerrada por el otro extremo.")
+    except Exception as e:
+        print(f"Error al enviar la imagen redimensionada: {e}")
+    finally:
+        sock.close()
+
+def servidor_redimension(ip, puerto, factor_redimension):
+    socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        socket_servidor.bind((ip, puerto))
+    except socket.error:
+        print("Error al vincular el socket.")
+        return
+
+    socket_servidor.listen(5)
+    print(f"Servidor escuchando en {ip}:{puerto}")
+
+    try:
+        while True:
+            socket_cliente, direccion = socket_servidor.accept()
+            print(f"Conexión aceptada desde {direccion[0]}:{direccion[1]}")
+
+            threading.Thread(target=manejar_cliente_redimension, args=(socket_cliente, factor_redimension)).start()
+    except KeyboardInterrupt:
+        print('Deteniendo el servidor.')
+    finally:
+        socket_servidor.close()
+
+def main():
+    analizador = argparse.ArgumentParser(description='Servidor de Redimension')
+    analizador.add_argument('-i', '--ip', required=True, help='Dirección de escucha')
+    analizador.add_argument('-p', '--puerto', type=int, required=True, help='Puerto de escucha')
+    analizador.add_argument('-s', '--escala', type=float, required=True, help='Factor de redimension')
+    argumentos = analizador.parse_args()
+
+    try:
+        servidor_redimension(argumentos.ip, argumentos.puerto, argumentos.escala)
+    except KeyboardInterrupt:
+        print('Deteniendo el servidor.')
+
+if __name__ == '__main__':
+    main()
